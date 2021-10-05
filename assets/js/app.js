0cef98d364c8ab366f308148d5662f1ac2c7083d
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-10-05 12:52:47 -0400

    Add scroll to top functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,15 @@
 
 const currentYear = document.querySelector(".current-year");
 currentYear.innerText = new Date().getFullYear();
+
+// Scroll to top functionality
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
