252f784aac7aa303a7d4ada3961f2a4348a205a7
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-10-05 13:33:59 -0400

    Add menu functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
+// Navbar
+
+const burgerMenu = document.querySelector("#burger-menu"),
+    ul = document.querySelector("nav ul"),
+    nav = document.querySelector("nav");
+
+burgerMenu.addEventListener("click", () => {
+    ul.classList.toggle("show");
+})
+
+// Close Menu functionality
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+);
+
 // Copyright Year
 
 const currentYear = document.querySelector(".current-year");
